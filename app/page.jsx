0309c9230d515c7e8c28f56f2c6da8ff7d7c36b2
MalithaJeewaka <144432834+MalithaJeewaka@users.noreply.github.com>
2025-01-06 12:31:52 +0530

    css file and tailwind config done

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-violet-300 text-2xl">Hello</h1>
+    </div>
+  );
 }
